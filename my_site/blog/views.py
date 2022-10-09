352209ebb3507bf77b4b288ae060b405ab7dbd7e
Md. Al Amin Munna <54406774+mdalaminmunna@users.noreply.github.com>
2022-10-09 23:10:04 +0600

    template(error)

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,10 +1,11 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def starting_page(request):
-    pass
+    return render(request, "blog/index.html")
 
 
 def posts(request):
