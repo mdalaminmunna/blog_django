caaf7f551cb3bb484c0f3d35a9d0f7b3960e262a
Md. Al Amin Munna <54406774+mdalaminmunna@users.noreply.github.com>
2022-10-17 23:36:50 +0600

    linked pages

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="Starting-Page"),
     path("posts", views. posts, name="Posts-Page"),
-    path("posts/<slug:slug>", views.post_detail, name="Post-Detail-page")
+    path("posts/<slug:slug>", views.post_detail, name="Post-Detail-Page")
 ]
