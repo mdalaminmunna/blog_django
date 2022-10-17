6d28c1e9b5fb36106f2302b25a13c8f0c79ac486
Md. Al Amin Munna <54406774+mdalaminmunna@users.noreply.github.com>
2022-10-17 23:52:59 +0600

    dummy data

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,35 @@
+from datetime import date
 from django.shortcuts import render
+
+posts = [
+    {
+        "slug": "bird",
+        "images": "bird.jpg",
+        "author": "Crazy",
+        "date": date(2021, 7, 21),
+        "title": "Ulubulu",
+        "excert": "This is ulubulu!",
+        "content": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur suscipit, corporis voluptatum tenetur necessitatibus assumenda quod vero totam repellat, quidem dolores repellendus nobis eos recusandae doloremque eum dolorem vel culpa?"
+    },
+    {
+        "slug": "character",
+        "images": "anime.png",
+        "author": "Witch",
+        "date": date(2021, 10, 12),
+        "title": "Ulubulu",
+        "excert": "This is ulubulu!",
+        "content": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur suscipit, corporis voluptatum tenetur necessitatibus assumenda quod vero totam repellat, quidem dolores repellendus nobis eos recusandae doloremque eum dolorem vel culpa?"
+    },
+    {
+        "slug": "panda",
+        "images": "pansit.jpg",
+        "author": "Lazy",
+        "date": date(2022, 2, 15),
+        "title": "Ulubulu",
+        "excert": "This is ulubulu!",
+        "content": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur suscipit, corporis voluptatum tenetur necessitatibus assumenda quod vero totam repellat, quidem dolores repellendus nobis eos recusandae doloremque eum dolorem vel culpa?"
+    }
+]
 
 # Create your views here.
 
